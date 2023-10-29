a6e90916734b0ab5a82d929e6a729506fd473781
[Rajesh Natarajan] <[n_rajeshdit@yahoo.com]>
2023-10-30 00:53:43 +0800

    add

---
+++ literals.py
@@ -0,0 +1,21 @@
+age = 22
+AGE = 44
+
+age /= 2
+
+print (age)
+print (AGE)
+print(age + AGE)
+
+
+
+#What is the output of the following python code?
+y = 5
+y = "Jack"
+print(y)
+
+#What is the output of the following python code?
+amount = 4
+cost = 2
+cost += 2
+print(amount * cost)
