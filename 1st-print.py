736f5c65d4604e3d9afd679f019b731b1c198715
“[Rajesh <[n_rajeshdit@yahoo.com]>
2023-10-29 17:23:27 +0800

    updated

--- 1st-print.py
+++ 1st-print.py
@@ -5,4 +5,9 @@ print ("hello, \n  print next line")
 
 print ("hello","mister", end="! \n" ) # Adding ! end of the line and print in new line
 
-print ("hello", "world" "Hello" "Kiddies" , sep="," ,end="!" )
+print ("hello", "world" + " " "Hello" + "Kiddies" , sep="," ,end="!\n" )
+print("Hello" + " " "future" + " " "python!")
+
+print('My age is ' + "25")
+
+print(1, 2, 3, 4, sep='#', end='&')
