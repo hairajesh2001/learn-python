5c297935bdfcfc37f68d3becbb9f9e4c0a0207ba
Rajesh <n_rajeshdit@yahoo.com>
2023-10-29 23:11:59 +0800

    added typecasting

---
+++ type-casting.py
@@ -0,0 +1,10 @@
+# to receice input from prompt we can use input()
+age =input("My age is: " )
+
+#this input can be printed directly using print()
+#if this need to use in any calculation then we need use casttype which is int or boolean
+
+
+print(age)
+
+print( int(age) + 10)
